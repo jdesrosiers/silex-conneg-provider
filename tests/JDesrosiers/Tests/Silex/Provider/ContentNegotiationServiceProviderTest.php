f84b5cad84eeaa77572646e2de74cbba8ca93558
Jason Desrosiers <jdesrosi@gmail.com>
2013-08-03 16:53:53 -0700

    Removing JmsSerializerServiceProvider
    This service provider was moved to
    [jdesrosiers/silex-jms-serializer-provider](https://github.com/jdesrosiers/silex-jms-serializer-provider)

--- tests/JDesrosiers/Tests/Silex/Provider/ContentNegotiationServiceProviderTest.php
+++ tests/JDesrosiers/Tests/Silex/Provider/ContentNegotiationServiceProviderTest.php
@@ -1,12 +1,18 @@
 <?php
 
-namespace JDesrosiers\Tests\Services\Cart;
+namespace JDesrosiers\Tests\Silex\Provider;
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use JDesrosiers\Silex\Provider\ContentNegotiationServiceProvider;
-use JDesrosiers\Silex\Provider\JmsSerializerServiceProvider;
+use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
+use JMS\Serializer\Naming\SerializedNameAnnotationStrategy;
+use JMS\Serializer\SerializerBuilder;
 use Silex\Application;
+use Symfony\Component\HttpKernel\Client;
 
 require_once __DIR__ . "/../../../../../vendor/autoload.php";
+
+AnnotationRegistry::registerAutoloadNamespace("JMS\Serializer\Annotation", __DIR__ . "/../../../../../vendor/jms/serializer/src");
 
 class CartServiceTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,26 +21,82 @@ class CartServiceTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->app = new Application();
-        $this->app->register(new JmsSerializerServiceProvider(), array(
-            "serializer.srcDir" => dirname(__DIR__) . "/vendor/jms/serializer/src",
-            "serializer.cacheDir" => dirname(__DIR__) . "/cache",
-            "serializer.namingStrategy" => "IdenticalProperty",
-        ));
+
+        $serializerBuilder = SerializerBuilder::create()
+                ->setCacheDir(__DIR__ . "/../cache")
+                ->setPropertyNamingStrategy(new SerializedNameAnnotationStrategy(new IdenticalPropertyNamingStrategy()));
+
         $this->app->register(new ContentNegotiationServiceProvider(), array(
-             "conneg.serializer" => $this->app["serializer"],
+             "conneg.serializer" => $serializerBuilder->build(),
              "conneg.serializationFormats" => array("json", "xml", "yml"),
              "conneg.deserializationFormats" => array("json", "xml"),
              "conneg.defaultContentType" => "json",
         ));
     }
 
-    public function testConneg()
+    public function dataProviderNotAcceptableReturns406()
     {
-//        $client = new Client($this->app);
-//        $client->request("GET", "/foo");
-//        $response = $client->getResponse();
-//
-//        $this->assertEquals(200, $response->getStatusCode());
-//        $this->assertEquals("application/json", $response->headers->get("Content-Type"));
+        return array(
+            array(""),
+            array("text/html"),
+            array("image/jpeg"),
+            array("foo/bar"),
+        );
+    }
+
+    /**
+     * @dataProvider dataProviderNotAcceptableReturns406
+     */
+    public function testNotAcceptableReturns406($accept)
+    {
+        $this->app->get("/foo", function () {
+            return "";
+        });
+
+        $headers = array(
+            "HTTP_ACCEPT" => $accept
+        );
+
+        $client = new Client($this->app, $headers);
+        $client->request("GET", "/foo");
+
+        $response = $client->getResponse();
+
+        $this->assertEquals("406", $response->getStatusCode());
+        $this->assertEquals("", $response->getContent());
+//        $this->assertFalse($response->headers->has("Content-Type"));
+    }
+
+    public function dataProviderUnsupportedMediaTypeReturns415()
+    {
+        return array(
+            array("application/json", "image/jpeg", "application/json"),
+            array("application/xml", "image/jpeg", "text/xml; charset=UTF-8"),
+            array("text/xml; charset=UTF-8", "image/jpeg", "text/xml; charset=UTF-8"),
+        );
+    }
+
+    /**
+     * @dataProvider dataProviderUnsupportedMediaTypeReturns415
+     */
+    public function testUnsupportedMediaTypeReturns415($accept, $contentType, $expectedContentType)
+    {
+        $app = $this->app;
+        $this->app->post("/foo", function () use ($app) {
+            $app["conneg"]->deserializeRequest("array");
+        });
+
+        $headers = array(
+            "HTTP_ACCEPT" => $accept,
+            "CONTENT_TYPE" => $contentType,
+        );
+
+        $client = new Client($this->app, $headers);
+        $client->request("POST", "/foo", array(), array(), $headers, "");
+
+        $response = $client->getResponse();
+
+        $this->assertEquals("415", $response->getStatusCode());
+        $this->assertEquals($expectedContentType, $response->headers->get("Content-Type"));
     }
 }
